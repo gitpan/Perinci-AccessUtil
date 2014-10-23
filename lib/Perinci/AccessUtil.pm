9a0e487e28fe981f280e1c058ed8e2e8a318240f
perlancar <perlancar@gmail.com>
2014-10-23 11:10:16 +0000

    Import of PERLANCAR/Perinci-AccessUtil-0.03 from CPAN.
    gitpan-cpan-distribution: Perinci-AccessUtil
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         PERLANCAR/Perinci-AccessUtil-0.03.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Perinci/AccessUtil.pm
+++ lib/Perinci/AccessUtil.pm
@@ -1,7 +1,7 @@
 package Perinci::AccessUtil;
 
 our $DATE = '2014-10-23'; # DATE
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 use 5.010001;
 use strict;
@@ -9,7 +9,29 @@ use warnings;
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(strip_riap_stuffs_from_res);
+our @EXPORT_OK = qw(insert_riap_stuffs_to_res strip_riap_stuffs_from_res);
+
+sub insert_riap_stuffs_to_res {
+    my ($res, $def_ver, $nmeta) = @_;
+
+    $res->[3]{'riap.v'} //= $def_ver // 1.1;
+    if ($res->[3]{'riap.v'} >= 1.2) {
+        # do we need to base64-encode?
+        {
+            last if $res->[3]{'riap.result_encoding'};
+            if ($nmeta) {
+                last unless $nmeta->{result}{schema} &&
+                    $nmeta->{result}{schema}[0] eq 'buf';
+            }
+            last unless defined($res->[2]) && !ref($res->[2]) &&
+                $res->[2] =~ /[^\x20-\x7f]/;
+            require MIME::Base64;
+            $res->[2] = MIME::Base64::encode_base64($res->[2]);
+            $res->[3]{'riap.result_encoding'} = 'base64';
+        }
+    }
+    $res;
+}
 
 sub strip_riap_stuffs_from_res {
     my $res = shift;
@@ -57,7 +79,7 @@ Perinci::AccessUtil - Utility module for Riap client/server
 
 =head1 VERSION
 
-This document describes version 0.02 of Perinci::AccessUtil (from Perl distribution Perinci-AccessUtil), released on 2014-10-23.
+This document describes version 0.03 of Perinci::AccessUtil (from Perl distribution Perinci-AccessUtil), released on 2014-10-23.
 
 =head1 SYNOPSIS
 
@@ -67,6 +89,15 @@ This document describes version 0.02 of Perinci::AccessUtil (from Perl distribut
 =head1 DESCRIPTION
 
 =head1 FUNCTIONS
+
+=head2 insert_riap_stuffs_to_res($envres[, $def_ver, $nmeta]) => array
+
+Starting in Riap protocol v1.2, server is required to return C<riap.v> in result
+metadata. This routine does just that. In addition to that, this routine also
+encodes result with base64 when necessary.
+
+This routine is used by Riap network server libraries, e.g.
+L<Perinci::Access::HTTP::Server> and L<Perinci::Access::Simple::Server>.
 
 =head2 strip_riap_stuffs_from_res($envres) => array
 
